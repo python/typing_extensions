ddc7dec04e9174fcfa8d552944764e7f7d13a1d4
Alex Waygood <Alex.Waygood@Gmail.com>
2022-05-27 15:45:11 +0100

    Lint

--- src/_typed_dict_test_helper.py
+++ src/_typed_dict_test_helper.py
@@ -1,5 +1,6 @@
-from typing import TypeVar, Generic, Optional, T
+from typing import Generic, Optional, T
 from typing_extensions import TypedDict
+
 
 class FooGeneric(TypedDict, Generic[T]):
     a: Optional[T]
