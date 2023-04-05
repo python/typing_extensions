c661cde89a3dcf5c240c6e26b20ab14afce4b9e9
YouJiacheng <1503679330@qq.com>
2023-04-06 07:21:50 +0800

    Move builtin protocol whitelist to mapping instead of list (#128)
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- src/test_typing_extensions.py
+++ src/test_typing_extensions.py
@@ -1910,6 +1910,14 @@ class ProtocolTests(BaseTestCase):
         self.assertIsSubclass(B, Custom)
         self.assertNotIsSubclass(A, Custom)
 
+    def test_builtin_protocol_allowlist(self):
+        with self.assertRaises(TypeError):
+            class CustomProtocol(TestCase, Protocol):
+                pass
+
+        class CustomContextManager(typing.ContextManager, Protocol):
+            pass
+
     def test_no_init_same_for_different_protocol_implementations(self):
         class CustomProtocolWithoutInitA(Protocol):
             pass
