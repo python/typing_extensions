597c07acca2891e5e31b7530e97f667256af6031
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-11-29 09:39:53 -0800

    Update src/test_typing_extensions.py
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- src/test_typing_extensions.py
+++ src/test_typing_extensions.py
@@ -3520,10 +3520,7 @@ class TypedDictTests(BaseTestCase):
 
     def test_typeddict_errors(self):
         Emp = TypedDict('Emp', {'name': str, 'id': int})
-        if hasattr(typing, "ReadOnly"):
-            self.assertEqual(TypedDict.__module__, 'typing')
-        else:
-            self.assertEqual(TypedDict.__module__, 'typing_extensions')
+        self.assertEqual(TypedDict.__module__, 'typing_extensions')
         jim = Emp(name='Jim', id=1)
         with self.assertRaises(TypeError):
             isinstance({}, Emp)
