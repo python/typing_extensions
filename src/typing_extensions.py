0f400b8a54cc3ee7d58aaa838254482917cd8e3f
Daniel <github.blurry@9ox.net>
2024-11-26 18:26:31 +0100

    support module keyword for some python versions

--- src/typing_extensions.py
+++ src/typing_extensions.py
@@ -4088,6 +4088,9 @@ else:
                     module=module,
                     allow_special_forms=allow_special_forms,
                 )
+            # module was added with bpo-41249 before special forms (bpo-46539)
+            elif "__forward_module__" in typing.ForwardRef.__slots__:
+                type_ = typing._type_convert(value, module=module)
             else:
                 type_ = typing._type_convert(value)
         else:
