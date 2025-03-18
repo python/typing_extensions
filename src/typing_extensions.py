e9fe4623547b88aa0b110653b10d8abacfb9c0da
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-03-18 10:18:35 -0700

    lint

--- src/typing_extensions.py
+++ src/typing_extensions.py
@@ -878,7 +878,9 @@ def _ensure_subclassable(mro_entries):
     return inner
 
 
-_NEEDS_SINGLETONMETA = not hasattr(typing, "NoDefault") or not hasattr(typing, "NoExtraItems")
+_NEEDS_SINGLETONMETA = (
+    not hasattr(typing, "NoDefault") or not hasattr(typing, "NoExtraItems")
+)
 
 if _NEEDS_SINGLETONMETA:
     class SingletonMeta(type):
